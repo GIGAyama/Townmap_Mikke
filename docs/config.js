b88bb0d7bd2fb872e4939b8af4f312c7c2d63e41
GIGA山 <basstar.1102@gmail.com>
2026-07-23 21:30:41 +0900

    Update exec URLs and Google Client ID in config

--- docs/config.js
+++ docs/config.js
@@ -4,9 +4,9 @@
  */
 window.APP_CONFIG = {
   // デプロイT（教員ポータル / 実行するユーザー: ウェブアプリケーションにアクセスしているユーザー）の exec URL
-  execUrlT: "",
+  execUrlT: "https://script.google.com/macros/s/AKfycbzsqH3vBdOmHxdVQ6tlouBXuwWMrcnAcXBP01Sz2G_uW6p6jOMXC7eHRJRJV1vVmSYlDA/exec",
   // デプロイS（児童用 / 実行するユーザー: 自分＝アプリアカウント）の exec URL
-  execUrlS: "",
+  execUrlS: "https://script.google.com/macros/s/AKfycbzT_b-PH6Wnjmn-56qWOr6DfIqYZxwz6fDYT8nqqPkQ49xMdPhgq6G90eUnuv6uqCmpdg/exec",
   // GIS 用 OAuth クライアントID（ウェブアプリ種別）。GAS 側 ScriptProperties の sp_googleClientId と同じ値
-  googleClientId: ""
+  googleClientId: "457436236877-qi3j29l3ssgu5b6rbiv7k0r625f4q5ea.apps.googleusercontent.com"
 };
